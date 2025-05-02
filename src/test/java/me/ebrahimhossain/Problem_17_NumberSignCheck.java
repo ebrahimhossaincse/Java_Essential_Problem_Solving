dc24c26949f195ab5e2d13812f5cb665e236394f
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-05-02 07:20:16 -0400

    add

--- src/test/java/me/ebrahimhossain/Problem_17_NumberSignCheck.java
+++ src/test/java/me/ebrahimhossain/Problem_17_NumberSignCheck.java
@@ -1,2 +1,21 @@
 package me.ebrahimhossain;
 
+import java.util.Scanner;
+
+// Problem 17: Number Sign Check
+class Problem_17_NumberSignCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double num = scanner.nextDouble();
+
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+        scanner.close();
+    }
+}
