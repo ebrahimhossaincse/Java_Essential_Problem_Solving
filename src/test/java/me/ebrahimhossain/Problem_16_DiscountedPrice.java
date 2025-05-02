c4c6ca3193f2c5154b5d0ac67ed4251a640f4301
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-05-02 07:19:21 -0400

    add

--- src/test/java/me/ebrahimhossain/Problem_16_DiscountedPrice.java
+++ src/test/java/me/ebrahimhossain/Problem_16_DiscountedPrice.java
@@ -1,2 +1,18 @@
 package me.ebrahimhossain;
 
+import java.util.Scanner;
+
+// Problem 16: Discounted Price
+class Problem_16_DiscountedPrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter original price: ");
+        double price = scanner.nextDouble();
+        System.out.print("Enter discount percentage: ");
+        double discount = scanner.nextDouble();
+
+        double discountedPrice = price * (1 - discount / 100);
+        System.out.printf("Discounted price: %.2f\n", discountedPrice);
+        scanner.close();
+    }
+}
